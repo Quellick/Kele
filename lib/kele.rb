6d2d21e2adcfb92a188738167fb79a3c97cf1f43
Tucker Platt <tucker@bendcable.com>
2018-01-30 19:09:18 +0000

    init and auth complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,6 +3,7 @@ require 'httparty'
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
+  
 
   def initialize(email,password)
     response = self.class.post('/sessions', body: {
