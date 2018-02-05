62db5a639f7b7eb1d4262546738c1cfdeade3d77
Tucker Platt <tucker@bendcable.com>
2018-02-05 19:48:00 +0000

    added create_submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -51,5 +51,17 @@ class Kele
     puts response.body
   end
   
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", headers: { "authorization": @auth_token },
+      body: {
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "checkpoint_id": checkpoint_id,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+      },
+      headers: { "authorization" => @auth_token, content_type: "application/json" })
+  end
+  
   
 end
