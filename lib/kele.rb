93726fa73346bee3a178f4974e532a4e36f81649
Tucker Platt <tucker@bendcable.com>
2018-02-06 18:41:37 +0000

    fixed arguments order

--- lib/kele.rb
+++ lib/kele.rb
@@ -51,14 +51,14 @@ class Kele
     puts response.body
   end
   
-  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
     response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", headers: { "authorization": @auth_token },
       body: {
         "assignment_branch": assignment_branch,
         "assignment_commit_link": assignment_commit_link,
         "checkpoint_id": checkpoint_id,
         "comment": comment,
-        "enrollment_id": enrollment_id
+        "enrollment_id": enrollment_id # current_enrollment_id 3681
       })
     p response
   end
